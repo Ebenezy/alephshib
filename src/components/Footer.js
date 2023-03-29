b61199ea6e459113b61075c029ce8c49f84a646c
Ebenezer Ndukwe <ebnettech@gmail.com>
2023-03-29 08:48:15 +0100

    footer added, animations added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import telegram from '../images/icons/telegram.svg'
+import twitter from '../images/icons/twitter.svg'
 
 const Footer = () => {
     return (
-        <footer>
-            <p className='mb-0'>Copyright © 2023 Alephshib Coin. All Rights Reserved</p>
-        </footer>
+        <div>
+            <div className='text-center'>
+                <Container>
+                    <div className="d-flex justify-content-center mt-5 mb-3">
+                        <a href="http://t.me/alephshib" target="_blank" rel="noreferrer">
+                            <div className="circle">
+                                <img src={telegram}
+                                    alt="telegram"
+                                />
+                            </div>
+                        </a>
+                        <a href="http://twitter.com/alephshib" target="_blank" rel="noreferrer">
+                            <div className="circle">
+                                <img
+                                    src={twitter}
+                                    alt="twitter"
+                                />
+                            </div>
+                        </a>
+                    </div>
+                    <div className='mb-5'>
+                        <a href='mailto:help@alephshib.com' className='mail'>help@alephshib.com</a>
+                    </div>
+                </Container>
+            </div>
+            <footer>
+                <p className='mb-0'>Copyright © 2023 Alephshib Coin. All Rights Reserved</p>
+            </footer>
+        </div>
     )
 }
 
